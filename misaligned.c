a8774c29445c967bef260af48f3c8a70b4ddcad2
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-01-25 10:34:12 +0530

    Modified misaligned.c to contain assertions

--- misaligned.c
+++ misaligned.c
@@ -1,13 +1,48 @@
 #include <stdio.h>
 #include <assert.h>
 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+
+//Finding the number of minor and major colors
+int numOfMajorColors = sizeof(majorColor) / sizeof(majorColor[0]);
+int numOfMinorColors = sizeof(minorColor) / sizeof(minorColor[0]);
+
+//Define Color Pair structure
+typedef struct {
+    int pairNumber;
+    const char* majColor;
+    const char* minColor;
+} ColorPair;
+
+//Function to print the color map on console
+void printColorMap(int pairNumber, const char *majorColor, const char *minorColor ) {
+	printf("%d | %s | %s\n", d, s1, s2);
+}
+
+ColorPair getColorInfoFromPairNumber(int pairNumber) {
+    ColorPair colorPair;
+    colorPair.pairNumber = pairNumber;
+    colorPair.majorColor = MajorColorNames[(pairNumber / numOfMajorColors)];
+    colorPair.minorColor = MinorColorNames[(pairNumber % numOfMinorColors)];
+    return colorPair;
+}
+
+//Test function to assert the major and minor color obtained from pair number.
+void testPairNumberToColor(int pairNumber, const char* expectedMajorColor, const char* expectedMinorColor){
+    ColorPair colorPair = getColorInfoFromPairNumber(pairNumber);
+    assert(colorPair.pairNumber == pairNumber);
+    assert(colorPair.majorColor == expectedMajorColor);
+    assert(colorPair.minorColor == expectedMinorColor);
+}
+
+//Print and test the color map information
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            printColorMap(i * 5 + j, majorColor[i], minorColor[i]);
+            testPairNumberToColor(i * 5 + j, majorColor[i], minorColor[i]);
         }
     }
     return i * j;
