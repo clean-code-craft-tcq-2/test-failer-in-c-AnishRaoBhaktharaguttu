fbc3f4a1b6e3f2784de39f3d1f94a9bd349cd8b1
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-01-25 11:02:59 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -34,9 +34,9 @@ void alertInCelcius(float farenheit) {
 }
 
 int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    alertInCelcius(522.6);
+    networkAlertCheck(alertInCelcius(400.5));
+    networkAlertCheck(alertInCelcius(303.6));
+    networkAlertCheck(alertInCelcius(522.6));
     assert(alertFailureCount == 2);
     printf("%d alerts failed.\n", alertFailureCount);
     printf("All is well (maybe!)\n");
