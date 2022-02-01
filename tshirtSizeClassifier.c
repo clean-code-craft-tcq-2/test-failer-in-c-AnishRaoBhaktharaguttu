395163b56228391e027e9890731bf39d0b933356
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-01 09:23:13 +0530

    Update tshirtSizeClassifier.c

--- tshirtSizeClassifier.c
+++ tshirtSizeClassifier.c
@@ -3,11 +3,11 @@
 
 char getTshirtSizeBasedOnShoulderMeasurement(int measure_cms) {
     char sizeName = '\0';
-    if(measure_cms < SMALL_SIZE_LOWER_THRESHOLD) {
+    if(measure_cms > SMALL_SIZE_LOWER_THRESHOLD && measure_cms < MEDIUM_SIZE_LOWER_THRESHOLD) {
         sizeName = 'S';
     } else if(measure_cms > MEDIUM_SIZE_LOWER_THRESHOLD && measure_cms < MEDIUM_SIZE_UPPER_THRESHOLD) {
         sizeName = 'M';
-    } else if(measure_cms > MEDIUM_SIZE_UPPER_THRESHOLD) {
+    } else if(measure_cms > MEDIUM_SIZE_UPPER_THRESHOLD && measure_cms < LARGE_SIZE_UPPER_THRESHOLD) {
         sizeName = 'L';
     }
     return sizeName;
