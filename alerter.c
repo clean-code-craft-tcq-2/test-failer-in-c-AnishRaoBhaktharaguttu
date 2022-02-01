12963e7bfdd5c09f11a9bdb3894b7cbdcdd0e4c0
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-01 16:42:16 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -1,44 +1,22 @@
 #include <stdio.h>
-#include <assert.h>
+#include "alerter.h"
 
-#define THRESHOLD_TEMP 180
 int alertFailureCount = 0;
-
-int networkAlertStub(float celcius) {
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-    // Return 200 for ok
-    // Return 500 for not-ok
-    // stub always succeeds and returns 200
-    if(celcius<THRESHOLD_TEMP){
-        return 200;
-    }else{
-        return 500;
-    }
-}
 
 float convertFarenheitToCelcius(float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
     return celcius;
 }
 
-void alertInCelcius(float farenheit) {
-    float celcius = convertFarenheitToCelcius(farenheit);
-    int returnCode = networkAlertStub(celcius);
-    if (returnCode != 200) {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+void checkNetworkFailure(int returnCode) {
+	if (returnCode != 200) {
+		alertFailureCount += 1;
     }
 }
 
-int main() {
-    alertInCelcius(400.5);
-    alertInCelcius(303.6);
-    alertInCelcius(522.6);
-    assert(alertFailureCount == 2);
-    printf("%d alerts failed.\n", alertFailureCount);
-    printf("All is well (maybe!)\n");
-    return 0;
+void alertInCelcius(float farenheit, int (*funPtr_NetworkAlert) (float)) {
+	float celcius = farenheitToCelciusConvertor(farenheit);
+    // Passing network check stub as function pointer 
+	int returnCode = (*funPtr_NetworkAlert)(celcius);
+	checkNetworkFailure(returnCode);
 }
