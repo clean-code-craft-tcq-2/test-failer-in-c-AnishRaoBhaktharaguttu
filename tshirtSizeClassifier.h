00cce05dd64f06c6fc97f2297418a76dcef2d70e
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-01 09:23:09 +0530

    Update tshirtSizeClassifier.h

--- tshirtSizeClassifier.h
+++ tshirtSizeClassifier.h
@@ -5,3 +5,4 @@ extern char getTshirtSizeBasedOnShoulderMeasurement(int measure_cms);
 #define SMALL_SIZE_LOWER_THRESHOLD 35
 #define MEDIUM_SIZE_LOWER_THRESHOLD 38
 #define MEDIUM_SIZE_UPPER_THRESHOLD 42
+#define LARGE_SIZE_UPPER_THRESHOLD 48
