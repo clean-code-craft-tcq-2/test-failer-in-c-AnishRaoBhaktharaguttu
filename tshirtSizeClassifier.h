94ebcf83f08fad4c4b96f3259a18f7d54aca3575
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-01 09:05:39 +0530

    Create tshirtSizeClassifier.h

---
+++ tshirtSizeClassifier.h
@@ -0,0 +1,7 @@
+//Defining the function
+extern char getTshirtSizeBasedOnShoulderMeasurement(int measure_cms);
+
+//Define the size limits as macros
+#define SMALL_SIZE_LOWER_THRESHOLD 35
+#define MEDIUM_SIZE_LOWER_THRESHOLD 38
+#define MEDIUM_SIZE_UPPER_THRESHOLD 42
