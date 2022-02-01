e41c8f822a19bb66061bf208a98afe8026a86b04
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-01 13:46:33 +0530

    Update testPrintColorCodingManual.c

--- testPrintColorCodingManual.c
+++ testPrintColorCodingManual.c
@@ -4,9 +4,9 @@
 extern ColorPair* colorPairExt;
 
 void testPrintColorMap(int expectedPairNumber, char expectedMajorColor[], char expectedMinorColor[]) {
-	assert(colorPair[expectedPairNumber-1].pairNumber == expectedPairNumber);
-	assert(colorPair[expectedPairNumber-1].majColor == expectedMajorColor);
-	assert(colorPair[expectedPairNumber-1].minColor == expectedMinorColor);
+	assert(colorPairExt[expectedPairNumber-1].pairNumber == expectedPairNumber);
+	assert(colorPairExt[expectedPairNumber-1].majColor == expectedMajorColor);
+	assert(colorPairExt[expectedPairNumber-1].minColor == expectedMinorColor);
 }
 
 int main() {
