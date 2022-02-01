47726578286472198be0b389b6424a1d8114eb9a
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-01 09:13:15 +0530

    Create testTshirtSizeClassifier.c

---
+++ testTshirtSizeClassifier.c
@@ -0,0 +1,17 @@
+//Test file to test the size classifier implementations
+
+#include "tshirtSizeClassifier.h"
+#include <assert.h>
+
+//Test and assert if the size classifier is working fine
+void testTshirtSizeBasedOnShoulderMeasurement(int measure_cms, char expectedSize) {
+	assert(tshirtSizeBasedOnShoulderLength(measure_cms) == expectedSize);
+}
+
+int main() {
+	testTshirtSizeBasedOnShoulderMeasurement(30,'S');
+	testTshirtSizeBasedOnShoulderMeasurement(40,'M');
+	testTshirtSizeBasedOnShoulderMeasurement(42,'L');
+	testTshirtSizeBasedOnShoulderMeasurement(55,'\0');
+	return 0;
+}
