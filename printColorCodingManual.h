c727671d96965cf03a5832be7918a8bbbc129683
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-02-01 20:33:16 +0530

    Update printColorCodingManual.h

--- printColorCodingManual.h
+++ printColorCodingManual.h
@@ -6,5 +6,5 @@ typedef struct {
 } ColorPair;
 
 extern ColorPair* constructColorCodingMap(ColorPair* colorPair);
-extern void printColorMapOnConsole(ColorPair* colorPair);
+extern void printColorMapOnConsole(ColorPair* colorPairExt);
 extern void printColorMap(void (*funPtr_printColorMapOnConsole)(ColorPair*));
